df6467532a5488dcf6361e64cc042d8e2a4a4758
SkewPL <piotrek54pl@gmail.com>
2016-06-04 13:36:10 +0200

    Totally new look and new backend

--- run.py
+++ run.py
@@ -2,79 +2,79 @@
 import cherrypy, os, json, sqlite3, time, datetime
 from jinja2 import Environment, FileSystemLoader
 
+from sensors import Sensor, Sensors
+
+class Core(object):
+    def __init__(self):
+        # Create databse
+        with sqlite3.connect("db.sqlite") as conn:
+            conn.execute(
+                """CREATE TABLE IF NOT EXISTS sensors(sid INTEGER PRIMARY KEY, token TEXT, description TEXT, updated INTEGER)""")
+            conn.execute(
+                """CREATE TABLE IF NOT EXISTS readings(sid, updated INT, value FLOAT, battery FLOAT)""")
+
+        # Create and read sensors from database
+        self.sensors = Sensors()
+        self.sensors.load()
+
+        # Create website
+        cherrypy.config.update({
+            "server.socket_port": 5000,
+            "server.socket_host": "0.0.0.0"
+        })
+        cherrypy.quickstart(WebRoot(self), "/", {
+            "/static": {
+                "tools.staticdir.root": os.getcwd(),
+                "tools.staticdir.on": True,
+                "tools.staticdir.dir": "static"
+            }
+        })
 
 class WebRoot(object):
 
-    def __init__(self):
+    def __init__(self, core):
         # Load templates
         self.env = Environment(loader=FileSystemLoader('templates'))
         def to_json(value): return json.dumps(value)
         self.env.filters["to_json"] = to_json
 
-        # Create place for current values
-        self.sensors = {}
-
-        # Create databse
-        with sqlite3.connect("db.sqlite") as conn:
-            conn.execute("""CREATE TABLE IF NOT EXISTS sensors(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                date INTEGER,
-                sensorid INTEGER,
-                value FLOAT
-            )""")
+        # Save core object
+        self.core = core
 
     @cherrypy.expose
     def index(self):
-        with sqlite3.connect("db.sqlite") as conn:
-            result = conn.execute("SELECT * FROM sensors WHERE date > ?",[time.time()-86400]).fetchall()
-            values = {}
+        return self.env.get_template('home.html').render(sensors=self.core.sensors.sensors)
 
-            # Create all sensors
-            for sensor in result:
-                if not sensor[2] in values:
-                    current = 0.0
-                    lastupd = "Never"
-                    if sensor[2] in self.sensors:
-                        current = self.sensors[sensor[2]]["current"]
-                        lastupd = self.sensors[sensor[2]]["lastupd"]
-                    values[sensor[2]] = {"id": sensor[2],"labels": [], "values": [], "current": current, "lastupd": lastupd}
+    @cherrypy.expose
+    def sensors(self, *args, **kwargs):
+        if "add" in kwargs:
+            try:
+                self.core.sensors.add(int(kwargs["sid"]),kwargs["description"])
+                return self.env.get_template('sensors.html').render(sensors=self.core.sensors.sensors,msg="Sensor added")
+            except sqlite3.IntegrityError:
+                return self.env.get_template('sensors.html').render(sensors=self.core.sensors.sensors,
+                                                                    msg="Sensor with that ID already exist")
+        if "remove" in kwargs:
+            if self.core.sensors.remove(int(kwargs["sid"])):
+                return self.env.get_template('sensors.html').render(sensors=self.core.sensors.sensors,msg="Sensor removed")
+            else:
+                return self.env.get_template('sensors.html').render(sensors=self.core.sensors.sensors,msg="Failed to remove sensor")
 
-            # Add all values to sensors
-            for sensor in result:
-                date = datetime.datetime.fromtimestamp(sensor[1])
-                print(date)
-                values[sensor[2]]["labels"].append("{}:{}".format(date.hour,date.minute))
-                values[sensor[2]]["values"].append(sensor[3])
-
-            return self.env.get_template('home.html').render(sensors=list(values.values()))
+        return self.env.get_template('sensors.html').render(sensors=self.core.sensors.sensors)
 
     @cherrypy.expose
     def api(self, *args, **kwargs):
-        if args[0] == "0" and kwargs["token"] == "123":
-            # All okay, get data
-            self.sensors[0] = {"current": float(kwargs["value"]), "lastupd": datetime.datetime.now().strftime("%d.%m.%Y %H:%M")}
-            with sqlite3.connect("db.sqlite") as conn:
-                conn.execute("INSERT INTO sensors (date, sensorid, value) VALUES (?,?,?)",[time.time(),0,kwargs["value"]])
-                return json.dumps({"success": "Value saved"})
+        print(args)
+
+        sensor = self.core.sensors.get(int(args[0]))
+        if sensor:
+            if sensor.token == kwargs["token"]:
+                sensor.update(float(kwargs["value"]),float(kwargs["battery"]))
+                return json.dumps({"success": 1, "message": "Sensor updated"})
+            else:
+                return json.dumps({"error": 101, "message": "Wrong token"})
         else:
-            return json.dumps({"error": "Wrong token"})
+            return json.dumps({"error": 100, "message": "Sensor with that id does not exist"})
 
 if __name__ == "__main__":
-    cherrypy.config.update({
-        "server.socket_port": 5000,
-        "server.socket_host": "0.0.0.0"
-    })
-    cherrypy.tree.mount(WebRoot(), "/", {
-        "/static": {
-            "tools.staticdir.root": os.getcwd(),
-            "tools.staticdir.on": True,
-            "tools.staticdir.dir": "static"
-        }
-    })
-    cherrypy.engine.signals.subscribe()
-    cherrypy.engine.start()
-
-    try:
-        while True: pass
-    except:
-        cherrypy.engine.exit()
+    Core()
---
+++ sensors.py
@@ -0,0 +1,95 @@
+from datetime import datetime, timedelta
+import time
+import sqlite3
+import random, string
+
+class Reading(object):
+
+    def __init__(self, sid, updated, value, battery):
+        self.sid = sid
+        self.updated = updated
+        self.value = value
+        self.battery = battery
+
+class Sensor(object):
+
+    def __init__(self, sid, token, description, value=None, battery=None):
+        self.sid = sid
+        self.updated = datetime.now()
+        self.token = token
+        self.description = description
+        self.value = value
+        self.battery = battery
+
+    def update(self, value, battery):
+        # Update values in database
+        with sqlite3.connect("db.sqlite") as conn:
+            conn.execute("UPDATE sensors SET updated=? WHERE sid=?", [int(time.time()), self.sid])
+            conn.execute("INSERT INTO readings (sid, updated, value, battery) VALUES (?,?,?,?);",
+                         [self.sid,int(time.time()),value,battery])
+
+        # Update values in object
+        self.value = value
+        self.battery = battery
+        self.updated = datetime.now()
+
+    def get_readings(self, delta=None):
+        with sqlite3.connect("db.sqlite") as conn:
+            if delta: updated = time.time()-delta
+            else: updated = 0
+            results = conn.execute("SELECT updated, value, battery FROM readings WHERE sid=? AND updated > ?;",[self.sid,updated]).fetchall()
+
+            readings = []
+            for result in results:
+                readings.append(Reading(self.sid, result[0],result[1],result[2]))
+            return readings
+
+class Sensors(object):
+
+    def __init__(self):
+        self.sensors = []
+
+    def load(self):
+        """Load all sensors from database"""
+        with sqlite3.connect("db.sqlite") as conn:
+            results = conn.execute("SELECT sid, token, description FROM sensors GROUP BY sid").fetchall()
+            for result in results: self.sensors.append(Sensor(result[0],result[1],result[2]))
+
+    def add(self, sid, description=None, token=None):
+        """Add sensor to database"""
+        if token == None: token = ''.join(random.choice(string.ascii_uppercase+string.digits) for _ in range(16))
+        if description == None: description = "Default description"
+
+        with sqlite3.connect("db.sqlite") as conn:
+            conn.execute("INSERT INTO sensors (sid, token, description, updated) VALUES (?,?,?,?)",
+                                   [sid,token,description,int(time.time())]).fetchall()
+            self.sensors.append(Sensor(sid,token,description))
+
+    def remove(self,sid):
+        sensor = self.get(sid)
+        if sensor:
+            with sqlite3.connect("db.sqlite") as conn:
+                conn.execute("DELETE FROM sensors WHERE sid=?",[sid])
+                conn.execute("DELETE FROM readings WHERE sid=?", [sid])
+                self.sensors.remove(sensor)
+                return True
+        return False
+
+    def get(self, sid):
+        for index, sensor in enumerate(self.sensors):
+            if sensor.sid == sid:
+                return self.sensors[index]
+        return None
+
+    def get_readings(self, delta=None):
+        readings = {}
+        for sensor in self.sensors:
+            readings[sensor.sid] = sensor.get_readings(delta)
+        return readings
+
+    def update(self, sid, value, battery):
+        sensor = self.get(sid)
+        if sensor:
+            sensor.update(value,battery)
+            return True
+        return False
